f926a99572573be3a80380a228eb3afc6dc102f7
Craig Sheppard <csheppard@site2020.com>
2025-02-01 14:12:14 -0400

    Initial Commit

---
+++ scoreboard/scoreboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  scoreboardApp.swift
+//  scoreboard
+//
+//  Created by Craig Sheppard on 2025-02-01.
+//
+
+import SwiftUI
+
+@main
+struct scoreboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
